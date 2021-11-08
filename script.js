6454fa8c2dd3ddb33b5a5008fe29cb014ec46e8d
setyofp <setyofajar@gmail.com>
2021-11-08 23:57:25 +0700

    portfolio webpage v1.0

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 const navLink = document.querySelectorAll('.nav-link');
+// const body = document.getElementsByTagName('BODY')[0];
 
 hamburger.addEventListener('click', mobileMenu);
 
 function mobileMenu() {
 	hamburger.classList.toggle('active');
 	navMenu.classList.toggle('active');
+	// body.classList.toggle('active');
 }
 
 navLink.forEach((n) => n.addEventListener('click', closeMenu));
@@ -14,4 +16,5 @@ navLink.forEach((n) => n.addEventListener('click', closeMenu));
 function closeMenu() {
 	hamburger.classList.remove('active');
 	navMenu.classList.remove('active');
+	// body.classList.toggle('active');
 }
